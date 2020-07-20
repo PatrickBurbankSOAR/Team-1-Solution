d532e42bac49c1008fca1f80c69f9acf3ba0bdf0
Melissa <melissa.utz@cgi.com>
2020-07-20 14:32:39 -0400

    Set up routing

--- IdeaApp/src/app/app-routing.module.ts
+++ IdeaApp/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from '../app/components/login-page/login-page.component';
+import { RegisterPageComponent } from '../app/components/register-page/register-page.component';
+import { ProfilePageComponent } from '../app/components/profile-page/profile-page.component';
+import { LandingPageComponent } from '../app/components/landing-page/landing-page.component';
+import { SubmitIdeaPageComponent } from '../app/components/submit-idea-page/submit-idea-page.component';
+import { IdeaCardComponent } from '../app/components/idea-card/idea-card.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: LoginPageComponent },
+  { path: 'login-page', component: LoginPageComponent },
+  { path: 'register-page', component: RegisterPageComponent },
+  { path: 'profile-page', component: ProfilePageComponent },
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'submit-idea-page', component: SubmitIdeaPageComponent },
+  { path: 'idea-card', component: IdeaCardComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- IdeaApp/src/app/app.module.ts
+++ IdeaApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
@@ -24,7 +24,8 @@ import { SubmitIdeaPageComponent } from './components/submit-idea-page/submit-id
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
