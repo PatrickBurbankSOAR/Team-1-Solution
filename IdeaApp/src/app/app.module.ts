667c50c2942121a87a2e929239c6a83b9f445dee
Melissa <melissa.utz@cgi.com>
2020-07-20 16:41:00 -0400

    Made profiles changes

--- IdeaApp/src/app/app.module.ts
+++ IdeaApp/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LandingPageComponent } from './components/landing-page/landing-page.com
 import { IdeaCardComponent } from './components/idea-card/idea-card.component';
 import { SubmitIdeaPageComponent } from './components/submit-idea-page/submit-idea-page.component';
 import { AddRegistrationComponent } from './components/Register-Page/add-registration/add-registration.component';
+import { } from '@angular/core'
 
 
 @NgModule({
--- IdeaApp/src/app/components/profile-page/profile-page.component.ts
+++ IdeaApp/src/app/components/profile-page/profile-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from './user';
-
+// import { User } from './user';
+import { Profile } from 'src/app/Models/Profile-Info'
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
@@ -8,14 +8,23 @@ import { User } from './user';
 })
 export class ProfilePageComponent implements OnInit {
 
-  user: User = {
+  // user: User = {
+  //   name: 'myUser',
+  //   email: 'myUser@gmail.com',
+  //   phone: '555-555-5555',
+  //   bio: 'Looking to share great new ideas!',
+  //   createdIdeas: 'Cool Idea #1',
+  //   likedIdeas: "Someone Else's Cool Idea"
+  // };
+
+  profile: Profile = {
+
     name: 'myUser',
+    phone: 5555555555,
     email: 'myUser@gmail.com',
-    phone: '555-555-5555',
-    bio: 'Looking to share great new ideas!',
-    createdIdeas: 'Cool Idea #1',
-    likedIdeas: "Someone Else's Cool Idea"
-  };
+    bio: 'Looking to create and view cool new ideas.'
+  }
+
 
 
   constructor() { }
