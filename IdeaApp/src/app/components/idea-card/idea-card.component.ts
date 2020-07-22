7b7026df1d21d0200b3bdb1b1e598b837dcb72c2
Ava Chong <33074700+avachong@users.noreply.github.com>
2020-07-22 14:58:29 -0400

    liked card function updated

--- IdeaApp/src/app/components/idea-card/idea-card.component.ts
+++ IdeaApp/src/app/components/idea-card/idea-card.component.ts
@@ -14,7 +14,6 @@ import {CardServiceService} from 'src/app/services/card-service.service';
 
 })
 export class IdeaCardComponent implements OnInit {
-<<<<<<< HEAD
   // cards = CARDSTACK; 
   ideas: string[];
 
@@ -28,16 +27,6 @@ export class IdeaCardComponent implements OnInit {
   // }
 
   constructor(private router: Router, private likesservice: LikesService, public cardservice: CardServiceService) { }
-=======
-  cards = CARDSTACK; 
-  ideas: string[] = [];
-  // public card: Card[] = CARDSTACK;
-  public index:number = 0;
-  userProfilePic:string;
-
-
-  constructor(private router: Router, private likesservice: LikesService) { }
->>>>>>> 5341aa0fc4e6d7a7994a0f209a406a09fd95ac5c
 
   ngOnInit(): void { 
     this.userProfilePic = 'assets/images/profilePicture.jpg'
@@ -66,7 +55,7 @@ export class IdeaCardComponent implements OnInit {
 
   likeCard() {
     this.cardservice.shiftCard();
-    const name = this.cards[this.index].title;
+    const name = this.cardservice.cards[0].title;
     this.ideas.push(name);
     this.likesservice.save(this.ideas);
 
